81a609bab06ac7f4a405eb28f4afd1b3262d18bf
andobrucki <annadobrucki@gmail.com>
2024-05-23 12:44:18 +0200

    changes on index.js

--- index.js
+++ index.js
@@ -1,47 +1,35 @@
 // Bulls and Cows
-// Setting up chalk and readline Sync
+// Setting up chalk and prompt
 const chalk = require('chalk');
 const prompt = require('prompt-sync')({ sigint: true });
 
 let name = prompt(chalk.magenta('What is your name? '));
-// let lengthSecretNum = prompt(
-// 	chalk.magenta('What is your favorite number between 2 and 6? ')
-//);
 
 // Function for choosing level
-let lengthSecretNum = 0;
-let level = 0;
-
 function levelChooser() {
+	let lengthSecretNum = 0;
 	let levelInput = prompt(
 		chalk.black.yellow(
-			`Ok, ${name}? Which level would you like to play? Easy(1), medium(2) or hard(3)? `
+			`Ok, ${name}! Which level would you like to play? Easy(1), medium(2) or hard(3)? `
 		)
 	);
-	if (levelInput == "1") {
-		console.log(`Ok, easy for you.`)
-		level = 1;
+	if (levelInput == '1') {
+		console.log(`Ok, easy for you.`);
 		lengthSecretNum = 2;
-	}
-	else if (levelInput === "2") {
-		console.log(`Medium, of course.`)
-		level = 2;
+	} else if (levelInput === '2') {
+		console.log(`Medium, of course.`);
 		lengthSecretNum = 3;
-	}
-	else if (levelInput === "3") {
-		console.log(`Hard, alright.`)
-		level = 3;
+	} else if (levelInput === '3') {
+		console.log(`Hard, alright.`);
 		lengthSecretNum = 4;
-	}
-	else {
-		console.log(`Invalid input, please enter a number between 1 and 3.`)
+	} else {
+		console.log(`Invalid input, please enter a number between 1 and 3.`);
+		levelChooser();
 	}
 	return lengthSecretNum;
 }
 
-levelChooser();
-
-//Function > Computer comes up with secret number
+//Function > Creating secret number
 function createSecretNumber(length) {
 	const digits = [];
 	while (digits.length < length) {
@@ -53,17 +41,18 @@ function createSecretNumber(length) {
 	return digits.join('');
 }
 
-const secretNumber = createSecretNumber(lengthSecretNum); // might change that to length according to level
+const secretNumber = createSecretNumber(levelChooser());
 
 // Function > ask if user knows the rules
+// If the user knows the rules she starts playing right away 
+// If she doesn't showInstructions() based on the secretNumber length
 function askForTheRules() {
 	let question = prompt(
 		chalk.black.yellow(`Do you know the rules of the game ${name}? Y/N: `)
 	);
 	console.clear();
 	if (question.toUpperCase() === 'N') {
-		levelChooser();
-		showInstructions(secretNumber.length); // here level acoording to user input
+		showInstructions(secretNumber.length);
 	} else {
 		console.log(chalk.yellow(`\nLet´s go, ${name}`));
 	}
@@ -72,28 +61,8 @@ askForTheRules();
 
 // Function > show instructions
 function showInstructions(len) {
-	console.log();
-	console.log(chalk.blueBright('Bulls 🐂 and Cows 🐄 Game'));
-	console.log(chalk.blueBright('-------------------'));
-	console.log(
-		chalk.blueBright(
-			'  You must guess the ' + len + ' digit number I am thinking of.'
-		)
-	);
-	console.log(chalk.blueBright('  The number is composed of the digits 1-9.'));
-	console.log(chalk.blueBright('  No digit appears more than once.'));
-	console.log(
-		chalk.blueBright('  After each of your guesses, I will tell you:')
-	);
-	console.log(
-		chalk.blueBright('    The number of bulls (digits in right place)')
-	);
-	console.log(
-		chalk.blueBright(
-			'    The number of cows (correct digits, but in the wrong place)'
-		)
-	);
-	console.log();
+	let instructions = `Bulls 🐂 and Cows 🐄 Game\n-------------------\n Acoording to your level choice you must guess the ${len} digit number I am thinking of. \n The number is composed of the digits 1-9.\n No digit appears more than once.\n After each of your guesses, I will tell you:\n The number of 🐂 bulls (digits in right place)\n The number of 🐄 cows (correct digits, but in the wrong place)\n `;
+	console.log(chalk.blue(instructions));
 }
 
 // Function Compare users guess with secret number
@@ -123,12 +92,9 @@ function randomMessage() {
 		'Try again',
 		'Not your day, give it another try',
 		'Roll the dice again!',
-		'Do not worry, give it another try',
 	];
-
 	let randomMessage =
 		randomMessages[Math.floor(Math.random() * randomMessages.length)];
-
 	return randomMessage;
 }
 
@@ -144,6 +110,7 @@ function playAgain() {
 		);
 	} else console.log(chalk.yellow(`Invalid input.`));
 }
+
 // Function Play the Game
 function playTheGame() {
 	let attempts = 0;
@@ -153,7 +120,6 @@ function playTheGame() {
 		if (!wrongInputAlert(secretNumber, guessedNumber)) {
 			continue; //refer to function wrongInputAlert that checks whether guessed input is valid / if yes, continue
 		}
-
 		if (secretNumber === guessedNumber) {
 			console.log(
 				chalk.magentaBright(
@@ -162,7 +128,6 @@ function playTheGame() {
 			);
 			break; //Exit when secret number and guessed number match
 		}
-
 		// now setting up cows and bulls
 		// if the number is in the right position it will count a bull
 		// if the number is right but in the wrong position it will count a cow
@@ -181,7 +146,9 @@ function playTheGame() {
 		if (bulls === 0 && cows === 0) {
 			console.log(chalk.red(randomMessage()));
 		} else {
-			console.log(chalk.blue(`You found ${cows} cows and ${bulls} bulls.`));
+			console.log(
+				chalk.blue(`You found ${cows} 🐄 cows and ${bulls} 🐂 bulls.`)
+			);
 		}
 	}
 }
